16457d69fd7dfe56d67b32045f5c147af81c0906
rexypratam4 <ignasiusrexy31@gmail.com>
2023-08-22 22:58:41 +0800

    PHP Keenam

---
+++ perbandingan.php
@@ -0,0 +1,48 @@
+<?php
+    $angka1 = 9;
+    $angka2 = 12;
+    if($angka1 > $angka2){
+        echo 'Betul';
+    }else{
+        echo 'Salah';
+    }
+    echo '<br>';
+
+    $angka1 = 9;
+    $angka2 = 12;
+    if($angka1 < $angka2){
+        echo 'Betul';
+    }else{
+        echo 'Salah';
+    }
+    echo '<br>';
+    $angka1 = 9;
+    $angka2 = 12;
+    if($angka1 <= $angka2){
+        echo 'Betul';
+    }else{
+        echo 'Salah';
+    }
+    echo '<br>';
+
+    $angka1 = 9;
+    $angka2 = 12;
+    if($angka1 >= $angka2){
+        echo 'Betul';
+    }else{
+        echo 'Salah';
+    }
+    echo '<br>';
+
+    $angka1 = 9;
+    $angka2 = 12;
+    if($angka1 == $angka2){
+        echo 'Betul';
+    }else{
+        echo 'Salah';
+    }
+    echo '<br>';
+
+
+
+

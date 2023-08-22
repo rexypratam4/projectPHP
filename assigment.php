867a2740a6bda97b4a1eb0be0ecc235440142e86
rexypratam4 <ignasiusrexy31@gmail.com>
2023-08-22 22:38:22 +0800

    PHP Kelima

---
+++ assigment.php
@@ -0,0 +1,39 @@
+<?php
+    $samaDengan = 10;
+    $samaDengan =  5;
+    echo $samaDengan;
+
+
+    $plusSamaDengan = 10;
+    $plusSamaDengan +=  5;
+    echo $plusSamaDengan;
+
+    $minusSamaDengan = 10;
+    $minusSamaDengan -=  5;
+    echo $minusSamaDengan;
+
+    $kaliSamaDengan = 10;
+    $kaliSamaDengan *=  5;
+    echo $kaliSamaDengan;
+
+    $bagiSamaDengan = 10;
+    $bagiSamaDengan /=  5;
+    echo $bagiSamaDengan;
+
+    $bagiSamaDengan = 10;
+    $bagiSamaDengan /=  5;
+    echo $bagiSamaDengan;
+
+    $modulusSamaDengan = 10;
+    $modulusSamaDengan %=  4;
+    echo $modulusSamaDengan;
+
+    
+    $titikSamaDengan = 'Rexy ';
+    $titikSamaDengan .=  'Pratama';
+    echo $titikSamaDengan;
+
+
+
+
+

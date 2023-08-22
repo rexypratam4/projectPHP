cdb8ccf35fcabde665411b5e4e634e3152d52f55
rexypratam4 <ignasiusrexy31@gmail.com>
2023-08-22 21:32:31 +0800

    PHP Awal

---
+++ phpdalamhtml.php
@@ -0,0 +1,19 @@
+<?php
+$nama = "jhon Doe";
+$asal = "Makassar";
+$umur = "24 Tahun";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Hi, <?php echo $nama ; ?> </h1>
+    <h1> Hi, <?php echo $asal ; ?> </h1>
+    <h1> Hi, <?php echo $umur ; ?> </h1>
+
+</body>
+</html>

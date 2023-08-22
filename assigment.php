56abc5039e0b345b5cdb9f3ac2e1838eba2e4ef4
rexypratam4 <ignasiusrexy31@gmail.com>
2023-08-23 00:10:35 +0800

    PHP assignment

--- assigment.php
+++ assigment.php
@@ -2,31 +2,43 @@
     $samaDengan = 10;
     $samaDengan =  5;
     echo $samaDengan;
-
+    echo '<br>';
 
     $plusSamaDengan = 10;
     $plusSamaDengan +=  5;
     echo $plusSamaDengan;
+    echo '<br>';
 
+ 
     $minusSamaDengan = 10;
     $minusSamaDengan -=  5;
     echo $minusSamaDengan;
+    echo '<br>';
+
 
     $kaliSamaDengan = 10;
     $kaliSamaDengan *=  5;
     echo $kaliSamaDengan;
+    echo '<br>';
+
 
     $bagiSamaDengan = 10;
     $bagiSamaDengan /=  5;
     echo $bagiSamaDengan;
+    echo '<br>';
+
 
     $bagiSamaDengan = 10;
     $bagiSamaDengan /=  5;
     echo $bagiSamaDengan;
+    echo '<br>';
+
 
     $modulusSamaDengan = 10;
     $modulusSamaDengan %=  4;
     echo $modulusSamaDengan;
+    echo '<br>';
+
 
     
     $titikSamaDengan = 'Rexy ';

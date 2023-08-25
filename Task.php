979ee8dc3399a229de84587612a55fb0e121b010
rexypratam4 <ignasiusrexy31@gmail.com>
2023-08-25 23:28:48 +0800

    PHP task

--- Task.php
+++ Task.php
@@ -1,21 +1,30 @@
 <?php
     // Task 1
-    for( $i=0; $i<=15; $i++){
-        if($i %3 ==0 ){
-            echo "$i";
-        }
+    // for( $i=0; $i<=15; $i++){
+    //     if($i %3 ==0 ){
+    //         echo "$i";
+    //     }
 
-    }
+    // }
 
     // Task 2
 
-    for( $i=1; $i<=12; $i++){
-        if( $i % 3 == 0){
-            echo "Halo";
-        }else if($i % 5 == 0){
-            echo "Cek";
-        }else {
-            echo "$i";
+    // for( $i=1; $i<=12; $i++){
+    //     if( $i % 3 == 0){
+    //         echo "Halo";
+    //     }else if($i % 5 == 0){
+    //         echo "Cek";
+    //     }else {
+    //         echo "$i";
+    //     }
+    // }
+
+    // Task 3
+
+    for($i=1; $i<=6; $i++){
+        for($j=1; $j<=$i; $j++){
+             echo " * ";
         }
+        echo "<br>";
     }
     

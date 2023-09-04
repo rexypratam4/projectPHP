ed6db734f7a276eedc9760c6644d972dffaeb0fe
rexypratam4 <ignasiusrexy31@gmail.com>
2023-09-04 21:54:27 +0800

    PHP Switch

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+    $nilai = '90';
+    switch($nilai){
+        case 90;
+        echo 'Nilai A';
+        break;
+        case 90;
+        echo 'Nilai B';
+        break;
+        case 90;
+        echo 'Nilai C';
+        break;
+        default;
+        echo 'Tidak Ketiganya';
+        
+    }

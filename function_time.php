3b1fbd7a1fd44968a0bbccf33fd8f78041aed5d6
rexypratam4 <ignasiusrexy31@gmail.com>
2023-09-04 22:50:08 +0800

    PHP Function Time

--- function_time.php
+++ function_time.php
@@ -1,6 +1,6 @@
 <?php
      date_default_timezone_set('Asia/Makassar');
      echo "Menghitung 10 hari ke belakang <br>";
-     echo date("d m y", time()-60*60*24*10);
+     echo date("d M y", time()-60*60*24*10);
      echo " <br><br>Menghitung 10 hari ke depan <br>";
-     echo date("d m y", time()+60*60*24*10);
+     echo date("d M y", time()+60*60*24*10);

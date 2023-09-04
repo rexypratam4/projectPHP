5617c00671cf7c9105ae5e8a0b094b33880f7d3d
rexypratam4 <ignasiusrexy31@gmail.com>
2023-09-04 21:26:35 +0800

    PHP Ifelse

---
+++ ifelseifelse.php
@@ -0,0 +1,30 @@
+<?php
+    $nilai =30;
+    if( $nilai <50){
+        echo 'Nilai C';
+    }elseif($nilai >=20 && $nilai <=80){
+        echo 'Nilai B';
+    }else{
+        echo 'Nilai A';
+    }
+        echo '<br>';
+        echo 'If else terstruktur';
+        echo '<br> <br>';
+        
+
+    // 
+    
+    if( $nilai >50){
+        echo 'Nilai C';
+    }
+    if($nilai >=20 && $nilai <=80){
+        echo 'Nilai B';
+    }else{
+        echo 'Nilai A';
+    }
+        echo '<br>';
+        echo 'If else 2 pengecekan';
+
+
+
+    

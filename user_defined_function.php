db042b5296505ce6110101b92cb42e244e65c73a
rexypratam4 <ignasiusrexy31@gmail.com>
2023-09-05 21:53:32 +0800

    PHP user defined function

---
+++ user_defined_function.php
@@ -0,0 +1,35 @@
+<!-- user defined menggunakan function -->
+<?php
+function hello (){
+    return "My bro, how are you?";  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+<body>
+    <h1> <?= hello();?> </h1>   
+</body>
+</html>
+
+<!-- user defined menggunakan parameter -->
+<?php
+function hello ($name = "User"){
+    return "My bro, how are you?";  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+<body>
+    <h1> <?= hello("Rexy");?> </h1>   
+</body>
+</html>
